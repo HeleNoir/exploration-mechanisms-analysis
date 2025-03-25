01548722bfc1cda12f21e3ae2139891e3669d9cc
Helena Stegherr <helena.stegherr@informatik.uni-augsburg.de>
2025-03-25 16:14:40 +0100

    Try to fix scripts again

--- slurm/analyse_all.sh
+++ slurm/analyse_all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export dim=$1
+dim=$1
 
-sbatch slurm/run_analyses.sbatch "PSO"
-sbatch slurm/run_analyses.sbatch "PSO_RR"
-sbatch slurm/run_analyses.sbatch "SHADE"
-sbatch slurm/run_analyses.sbatch "PSO_GPGM"
-sbatch slurm/run_analyses.sbatch "PSO_NPGM"
-sbatch slurm/run_analyses.sbatch "PSO_PDM"
-sbatch slurm/run_analyses.sbatch "PSO_SRM"
+sbatch slurm/run_analyses.sbatch "PSO" $dim
+sbatch slurm/run_analyses.sbatch "PSO_RR" $dim
+sbatch slurm/run_analyses.sbatch "SHADE" $dim
+sbatch slurm/run_analyses.sbatch "PSO_GPGM" $dim
+sbatch slurm/run_analyses.sbatch "PSO_NPGM" $dim
+sbatch slurm/run_analyses.sbatch "PSO_PDM" $dim
+sbatch slurm/run_analyses.sbatch "PSO_SRM" $dim
--- slurm/convert_all.sh
+++ slurm/convert_all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export dim=$1
+dim=$1
 
-sbatch slurm/convert_logs.sbatch "PSO"
-sbatch slurm/convert_logs.sbatch "PSO_RR"
-sbatch slurm/convert_logs.sbatch "SHADE"
-sbatch slurm/convert_logs.sbatch "PSO_GPGM"
-sbatch slurm/convert_logs.sbatch "PSO_NPGM"
-sbatch slurm/convert_logs.sbatch "PSO_PDM"
-sbatch slurm/convert_logs.sbatch "PSO_SRM"
+sbatch slurm/convert_logs.sbatch "PSO" $dim
+sbatch slurm/convert_logs.sbatch "PSO_RR" $dim
+sbatch slurm/convert_logs.sbatch "SHADE" $dim
+sbatch slurm/convert_logs.sbatch "PSO_GPGM" $dim
+sbatch slurm/convert_logs.sbatch "PSO_NPGM" $dim
+sbatch slurm/convert_logs.sbatch "PSO_PDM" $dim
+sbatch slurm/convert_logs.sbatch "PSO_SRM" $dim
