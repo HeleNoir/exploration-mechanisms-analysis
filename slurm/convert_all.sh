485241d661f13bb6e197f865721c03c154f647b7
Helena Stegherr <helena.stegherr@informatik.uni-augsburg.de>
2025-03-25 16:01:16 +0100

    Try to fix scripts

--- slurm/convert_all.sh
+++ slurm/convert_all.sh
@@ -2,10 +2,10 @@
 
 export dim=$1
 
-sbatch slurm/convert_logs.sbatch "PSO" dim
-sbatch slurm/convert_logs.sbatch "PSO_RR" dim
-sbatch slurm/convert_logs.sbatch "SHADE" dim
-sbatch slurm/convert_logs.sbatch "PSO_GPGM" dim
-sbatch slurm/convert_logs.sbatch "PSO_NPGM" dim
-sbatch slurm/convert_logs.sbatch "PSO_PDM" dim
-sbatch slurm/convert_logs.sbatch "PSO_SRM" dim
+sbatch slurm/convert_logs.sbatch "PSO"
+sbatch slurm/convert_logs.sbatch "PSO_RR"
+sbatch slurm/convert_logs.sbatch "SHADE"
+sbatch slurm/convert_logs.sbatch "PSO_GPGM"
+sbatch slurm/convert_logs.sbatch "PSO_NPGM"
+sbatch slurm/convert_logs.sbatch "PSO_PDM"
+sbatch slurm/convert_logs.sbatch "PSO_SRM"
